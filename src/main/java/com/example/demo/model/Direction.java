f071571c99e547ba65ae209fb70da17731536169
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-21 11:44:06 +0530

    Add Direction ENUM and turn signature in NavigationService

---
+++ src/main/java/com/example/demo/model/Direction.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public enum Direction {
+
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
--- src/main/java/com/example/demo/service/NavigationService.java
+++ src/main/java/com/example/demo/service/NavigationService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Coordinate;
+import com.example.demo.model.Direction;
 
 public interface NavigationService {
 
     Coordinate move(boolean forward);
+
+    Direction turn(boolean left);
 }
