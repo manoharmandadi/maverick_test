8d2db2bc396c50999b737f71621d537d93eb800e
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-21 12:20:03 +0530

    Add probe initial location config to application.properties and set to the probe

--- src/main/java/com/example/demo/component/Probe.java
+++ src/main/java/com/example/demo/component/Probe.java
@@ -2,10 +2,21 @@ package com.example.demo.component;
 
 import com.example.demo.model.Coordinate;
 import com.example.demo.model.Direction;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Probe {
+
+    @Value("${probe.initial.x}")
+    private int x;
+
+    @Value("${probe.initial.y}")
+    private int y;
+
+    @Value("${probe.initial.direction")
+    private Direction direction;
+
 
     public Coordinate move(boolean forward){
         return null;
