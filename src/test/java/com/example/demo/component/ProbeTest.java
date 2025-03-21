db5b2a13674d532974061931164fe95a6f97178f
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-21 14:11:12 +0530

    Set Probe Fields with Reflection

--- src/test/java/com/example/demo/component/ProbeTest.java
+++ src/test/java/com/example/demo/component/ProbeTest.java
@@ -1,17 +1,27 @@
 package com.example.demo.component;
 
 import com.example.demo.model.Coordinate;
+import com.example.demo.model.Direction;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 public class ProbeTest {
 
-    @InjectMocks
     private Probe probe;
+
+
+    @BeforeEach
+    public void initProbe(){
+        probe = new Probe();
+        ReflectionTestUtils.setField(probe, "x", 10);
+        ReflectionTestUtils.setField(probe, "y", 10);
+        ReflectionTestUtils.setField(probe, "direction", Direction.EAST);
+    }
 
     @Test
     public void testMoveForward(){
--- src/test/java/com/example/demo/service/NavigationServiceTest.java
+++ src/test/java/com/example/demo/service/NavigationServiceTest.java
@@ -1,9 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.component.FloorGrid;
+import com.example.demo.model.Coordinate;
+import com.example.demo.model.Direction;
+import com.example.demo.model.GridElement;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -12,13 +18,12 @@ public class NavigationServiceTest {
     @InjectMocks
     private NavigationServiceImpl navigationService;
 
-    @Test
-    public void testMoveForward(){
-        Assertions.assertNotNull(navigationService.move(true));
-    }
+    @Mock
+    private FloorGrid floorGrid;
 
     @Test
-    public void testTurnLeft(){
-        Assertions.assertNotNull(navigationService.turn(true));
+    public void testMoveForward(){
+        Mockito.when(floorGrid.getElement(Mockito.any(Coordinate.class))).thenReturn(new GridElement(true, false));
+        Assertions.assertNotNull(navigationService.isNavigable(new Coordinate(3, 3), Direction.EAST, true));
     }
 }
