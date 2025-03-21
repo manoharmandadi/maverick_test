ff3f420c2aa43b8c5a9c4426c870a8089ac9b4d2
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-03-21 11:40:23 +0530

    Add Coordinate and NavigationService interface

---
+++ src/main/java/com/example/demo/service/NavigationService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Coordinate;
+
+public interface NavigationService {
+
+    Coordinate move(boolean forward);
+}
--- src/test/java/com/example/demo/controller/NavigationControllerTest.java
+++ src/test/java/com/example/demo/controller/NavigationControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
